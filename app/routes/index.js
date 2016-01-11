ec25863af177f6f7fac0374823b3ea57e1f8632d
Harvey Sanders <sanders.harvey@gmail.com>
2016-01-11 17:16:43 -0600

    add POST api route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+// var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var fileUploadHandler = require(path + '/app/controllers/fileUploadHandler.server.js');
+var fileUploadHandler = fileUploadHandler();
 
 module.exports = function (app, passport) {
 
@@ -13,45 +15,48 @@ module.exports = function (app, passport) {
 		}
 	}
 
-	var clickHandler = new ClickHandler();
-
+	// var clickHandler = new ClickHandler();
+	
 	app.route('/')
 		.get(isLoggedIn, function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+	app.route('/api/fileanalyze')
+		.post(fileUploadHandler.getFileSize);
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+	// app.route('/login')
+	// 	.get(function (req, res) {
+	// 		res.sendFile(path + '/public/login.html');
+	// 	});
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
+	// app.route('/logout')
+	// 	.get(function (req, res) {
+	// 		req.logout();
+	// 		res.redirect('/login');
+	// 	});
 
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
+	// app.route('/profile')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.sendFile(path + '/public/profile.html');
+	// 	});
 
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
+	// app.route('/api/:id')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.json(req.user.github);
+	// 	});
 
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
+	// app.route('/auth/github')
+	// 	.get(passport.authenticate('github'));
 
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	// app.route('/auth/github/callback')
+	// 	.get(passport.authenticate('github', {
+	// 		successRedirect: '/',
+	// 		failureRedirect: '/login'
+	// 	}));
+
+	// app.route('/api/:id/clicks')
+	// 	.get(isLoggedIn, clickHandler.getClicks)
+	// 	.post(isLoggedIn, clickHandler.addClick)
+	// 	.delete(isLoggedIn, clickHandler.resetClicks);
 };
